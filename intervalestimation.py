cd34fb7d5cb27de4d28fb37fce458c72ce80ea31
ichikawa-t0247 <98450933+ichikawa-t0247@users.noreply.github.com>
2022-02-14 12:54:48 +0900

    Update intervalestimation.py

--- intervalestimation.py
+++ intervalestimation.py
@@ -42,4 +42,4 @@ st.markdown(f'<center><font size=7 color="#FF4B00"> {bottom/visitors_a:.5f}～{u
 
 st.markdown('必要サンプル数')
 bottom, up = sp.stats.norm.interval(alpha, loc=0, scale=1)
-st.markdown(f'<center><font size=7 color="#FF4B00"> {up}</font></center>', unsafe_allow_html=True)
+st.markdown(f'<center><font size=7 color="#FF4B00"> {np.sqrt(p*(1-p))}</font></center>', unsafe_allow_html=True)
