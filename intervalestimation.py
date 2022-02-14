7462011d7efbda404cae33a703d549d924988cea
ichikawa-t0247 <98450933+ichikawa-t0247@users.noreply.github.com>
2022-02-14 11:03:29 +0900

    Update intervalestimation.py

--- intervalestimation.py
+++ intervalestimation.py
@@ -3,11 +3,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy import stats
+from scipy.stats import beta
 sns.set(font_scale=2)
 st.sidebar.markdown('区間推定(母比率)対象のデータを入力してください')
 visitors_a = st.sidebar.number_input('分母', value=100)
 conversion_a = st.sidebar.number_input('分子', value=50)
+alpha = st.sidebar.number_input('信頼度', value=0.95)
 cvr_a = conversion_a / visitors_a
 st.sidebar.markdown(f'比率: **{"{:.1%}".format(cvr_a)}**')
 
@@ -50,50 +51,13 @@ else:
     <center><font size=7 color="#FF4B00">有意差なし</font></center>
     ''', unsafe_allow_html=True)
 
-st.markdown('二項検定のABテストの結果。(サンプル少ない場合に利用)')
-data = np.matrix([ [ conversion_a, visitors_a ], [ conversion_b, visitors_b ] ])
-p_a = visitors_a / (visitors_a+visitors_b)
-conversion_total = np.sum(data, axis=0).item(0, 0)
-conversion_a  = data.item(0,0)
-p_value = stats.binom_test(x=conversion_a, n=conversion_total, p=p_a, alternative="two-sided")
-st.markdown(f'p値: **{"{:.4}".format(p_value)}**')
-if p_value <= 0.05:
-  st.markdown(r'''
-    <center><font size=7 color="#00B06B">95%の信頼度で有意差あり</font></center>
-    ''', unsafe_allow_html=True)
-elif p_value <= 0.1:
-  st.markdown(r'''
-    <center><font size=7 color="#F2E700">90%の信頼度で有意差あり</font></center>
-    ''', unsafe_allow_html=True)
-elif p_value <= 0.2:
-  st.markdown(r'''
-    <center><font size=7 color="#F2E700">80%の信頼度で有意差あり</font></center>
-    ''', unsafe_allow_html=True)
-else:
-  st.markdown(r'''
-    <center><font size=7 color="#FF4B00">有意差なし</font></center>
-    ''', unsafe_allow_html=True)
-st.subheader('ベイジアンABテスト')
-st.markdown('ベイズ推論を活用したABテスト。A, BそれぞれのCVRがどの程度信用できるのかを確認可能。')
-alpha_prior = 1
-beta_prior = 1
-posterior_A = stats.beta(alpha_prior + conversion_a, beta_prior + visitors_a - conversion_a)
-posterior_B = stats.beta(alpha_prior + conversion_b, beta_prior + visitors_b - conversion_b)
-samples = 450000
-samples_posterior_A = posterior_A.rvs(samples)
-samples_posterior_B = posterior_B.rvs(samples)
-prob = (samples_posterior_A < samples_posterior_B).mean()
-fig = plt.figure(figsize=(20,10))
-ax = fig.add_subplot(111)
-sns.distplot(samples_posterior_A, ax=ax, label='CVR of A')
-sns.distplot(samples_posterior_B, ax=ax, label='CVR of B')
-ax.set_ylabel('KDE', fontsize='xx-large')
-ax.set_xlabel('CVR', fontsize='xx-large')
-ax.set_title('distribution of CVR', fontsize='xx-large')
-ax.legend(loc='upper right', fontsize='xx-large')
-fig.tight_layout()
-st.subheader('CVRの信用度の分布')
-st.pyplot(fig)
-st.markdown(fr'''
-  <center><font size=7>CVRがA < Bとなる確率: {"{:.1%}".format(prob)}</font></center>
-  ''', unsafe_allow_html=True)
+st.markdown('ベータ分布を利用した母比率の区間推定。(サンプル少ない場合に利用)')
+alpha = 0.05 # 95%信頼区間
+visitors_a = 17        # 試行回数　
+conversion_a = 17        # 成功回数
+
+lower_limit = beta.ppf((1-alpha)/2, visitors_a, conversion_a-visitors_a+1)
+upper_limit = beta.ppf(1-(1-alpha)/2, visitors_a+1, conversion_a-visitors_a)
+print(f'95% Confidence interval: [{lower_limit:.5f}, {upper_limit:.5f}]')
+
+st.markdown(f'95% Confidence interval: [{lower_limit:.5f}, {upper_limit:.5f}]')
